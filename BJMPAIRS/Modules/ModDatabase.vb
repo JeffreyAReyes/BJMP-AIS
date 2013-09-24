cf11ce8ef5882366b82965a8f16f7abb15433f16
Jeffrey A. Reyes <jeofadriel@hotmail.com>
2013-09-24 21:20:47 +0700

    Added functions DLExecuteSPNonQuery to module ModDatabase

--- BJMPAIRS/Modules/ModDatabase.vb
+++ BJMPAIRS/Modules/ModDatabase.vb
@@ -22,7 +22,7 @@ Module ModDatabase
     ''' A generic method of executing sql statement. Accepts any number of parameters to pass to the sql statement.
     ''' Sept. 24, 2013 9:05 PM - Jeffrey A. Reyes
     ''' </remarks>
-    Public Function GetDataAndReturnDataSet(ByVal connString As String, ByVal commText As String, _
+    Public Function DLGetDataAndReturnDataSet(ByVal connString As String, ByVal commText As String, _
                 ByVal paramList As Dictionary(Of String, String), ByVal commType As Integer) As DataSet
 
         Dim collData As New DataSet
@@ -43,7 +43,7 @@ Module ModDatabase
                         Next
 
                     End With
-                    
+
                     myconn.Open()
 
                     myadapter.SelectCommand = mycomm
@@ -63,4 +63,57 @@ Module ModDatabase
 
     End Function
 
+    ''' <summary>
+    ''' Executes a sql statement. Returns boolean, True if successful.
+    ''' </summary>
+    ''' <param name="connString"></param>
+    ''' <param name="commText"></param>
+    ''' <param name="commType"></param>
+    ''' <param name="paramList"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' A generic method of executing sql statement. Accepts any number of parameters to pass to the sql statement.
+    ''' Sept. 24, 2013 9:19 PM - Jeffrey A. Reyes
+    ''' </remarks>
+    Public Function DLExecuteSPNonQuery(ByVal connString As String, ByVal commText As String, ByVal commType As Integer, ByVal paramList As Dictionary(Of String, String)) As Boolean
+
+        Dim boolReturn As Boolean
+
+        Try
+            Using myconn As New MySqlConnection(connString)
+
+                Using mycommand As New MySqlCommand
+                    With mycommand
+                        .Connection = myconn
+                        .CommandType = commType
+                        .CommandText = commText
+
+                        'iterate parameters
+                        For Each key In paramList.Keys
+                            .Parameters.AddWithValue(key, paramList(key))
+                        Next
+
+                        myconn.Open()
+
+                        mycommand.ExecuteNonQuery()
+
+                    End With
+                End Using
+            End Using
+            boolReturn = True
+        Catch ex As MySqlException
+
+            Debug.Print(ex.Message & vbCrLf & ex.StackTrace & vbCrLf & " MySqlException. In SIExecuteSPNonQuery." & strQuery)
+            boolReturn = False
+
+        Catch ex As Exception
+
+            Debug.Print(ex.Message & vbCrLf & ex.StackTrace & vbCrLf & " MySqlException. In SIExecuteSPNonQuery." & strQuery)
+
+            boolReturn = False
+        End Try
+
+        Return boolReturn
+
+    End Function
 End Module
